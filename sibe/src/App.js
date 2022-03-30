2def5e6d652d6c814426b52bf5548db4228d6d43
Amauri Klamas <jrklamas@hotmail.com>
2022-03-30 02:43:45 -0300

    ambiente front-end

---
+++ sibe/src/App.js
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from 'react'
+import "./index.css";
+import axios from 'axios';
+
+
+export default function App() {
+
+  const [todoList, setTodoList] = useState([])
+
+  useEffect(() => {
+    axios.get('http://localhost:8888/funcionario', {})
+      .then(res => {
+        console.log(res)
+        setTodoList(res.data)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }, [])
+  
+  const deleteHandler = id => {
+      const newTodos = todoList.filter(item => {
+          return item.id !== id
+      })
+
+      setTodoList(newTodos)
+  }
+
+  const updateHandler = todo => {
+      setTodoList(todoList.map(item => {
+          if(item.id === todo.id) {
+              return {
+                  ...item,
+                  message: todo.message
+              }
+          } else {
+              return item
+          }
+      }))
+  }
+
+  return (
+    <div class="form-container">
+      <form class="register-form">
+        <input
+          id="name"
+          class="form-field"
+          type="text"
+          placeholder="Nome"
+          name="nomeCompleto"
+        />
+        <input
+          id="logiin"
+          class="form-field"
+          type="text"
+          placeholder="Login"
+          name="login"
+        />
+        <input
+          id="senha"
+          class="form-field"
+          type="password"
+          placeholder="Senha"
+          name="senha"
+        />
+        <button class="form-field" type="submit">
+          Registrar
+        </button>
+      </form>
+    </div>
+  );
+}
