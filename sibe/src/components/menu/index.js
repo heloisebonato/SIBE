b6a4ef049089bae1cc3f17b1f36bc6495e8429a9
heloisebonato <43188658+heloisebonato@users.noreply.github.com>
2022-04-20 14:43:47 -0300

    modified sidebar menu

--- sibe/src/components/menu/index.js
+++ sibe/src/components/menu/index.js
@@ -1,38 +1,49 @@
 import React, {Component} from "react";
 import { NavLink } from "react-router-dom";
+import './menu.css'
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import PeopleIcon from '@mui/icons-material/People';
+import AppRegistrationIcon from '@mui/icons-material/AppRegistration';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import GroupsIcon from '@mui/icons-material/Groups';
 
 class Menu extends React.Component {
     render() {
         return (
             
-            <nav id="sidebarMenu" className="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse mt-5">
+            <nav id="sidebarMenu" className="sidebar col-md-3 col-lg-2 d-md-block bg-light collapse mt-5">
                 <div className="position-sticky pt-3">
                     <ul className="nav flex-column">
-                    <li className="nav-item">
-                        <NavLink to={'/'} exact className="nav-link">
-                                Dashboard
-                        </NavLink>
-                    </li>
-                    <li  className="nav-item">
-                        <NavLink to={'/funcionarios'} className="nav-link">
-                            Funcionarios
-                        </NavLink>
-                    </li>
-                    <li  className="nav-item">
-                        <NavLink to={'/registrar'} className="nav-link">
-                            Registrar
-                        </NavLink>
-                    </li>
-                    <li  className="nav-item">
-                        <NavLink to={'/cadastroCliente'} className="nav-link">
-                            Cadastro Cliente
-                        </NavLink>
-                    </li>
-                    <li  className="nav-item">
-                        <NavLink to={'/clientes'} className="nav-link">
-                            Clientes
-                        </NavLink>
-                    </li>
+                        <li className="nav-item">
+                            <NavLink to={'/'} exact className="nav-link">
+                                <DashboardIcon />
+                                    Dashboard
+                            </NavLink>
+                        </li>
+                        <li  className="nav-item">
+                            <NavLink to={'/funcionarios'} className="nav-link">
+                            <PeopleIcon />
+                                Funcionarios
+                            </NavLink>
+                        </li>
+                        <li  className="nav-item">
+                            <NavLink to={'/registrar'} className="nav-link">
+                            <AppRegistrationIcon />
+                                Registrar
+                            </NavLink>
+                        </li>
+                        <li  className="nav-item">
+                            <NavLink to={'/cadastroCliente'} className="nav-link">
+                            <AddCircleIcon />
+                                Cadastro Cliente
+                            </NavLink>
+                        </li>
+                        <li  className="nav-item">
+                            <NavLink to={'/clientes'} className="nav-link">
+                            <GroupsIcon />
+                                Clientes
+                            </NavLink>
+                        </li>
                     </ul>
                 </div>
             </nav>
